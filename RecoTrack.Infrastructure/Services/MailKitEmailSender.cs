70fda9ef8a76823ceec456a4d8108a13c8821b42
Piyush Singh <workspace.piyush01@gmail.com>
2025-11-04 17:35:33 +0530

    Removed duplicate options

--- RecoTrack.Infrastructure/Services/MailKitEmailSender.cs
+++ RecoTrack.Infrastructure/Services/MailKitEmailSender.cs
@@ -1,7 +1,6 @@
 ﻿using MailKit.Net.Smtp;
 using MailKit.Security;
 using Microsoft.Extensions.Options;
-using Microsoft.Extensions.Options;
 using MimeKit;
 using RecoTrack.Application.Interfaces;
 using RecoTrack.Application.Models;
