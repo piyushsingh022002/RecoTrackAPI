d3a57a447261ba57afd0c72b64efc5a01af4ee7b
Piyush Singh <workspace.piyush01@gmail.com>
2025-12-18 13:54:50 +0530

    Middlewares moved to custom Directory

---
+++ RecoTrackApi/CustomMiddlewares/CorrelationIdMiddleware.cs
@@ -0,0 +1,46 @@
+﻿using Serilog.Context;
+
+namespace RecoTrackApi.CustomMiddlewares
+{
+    public class CorrelationIdMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CorrelationIdMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            const string correlationHeader = "X-Correlation-Id";
+
+            //check if there is correlation id is already present in the Request Header or not
+            if(!context.Request.Headers.TryGetValue(correlationHeader, out var correlationId))
+            {
+                correlationId = Guid.NewGuid().ToString();
+            }
+
+            //Adding Optional Configurable Prefix(Advance)
+            var prefix = context.RequestServices.GetService<IConfiguration>()
+                ?.GetValue<string>("CorrelationIdPrefix") ?? "";
+            var correlationIdValue = prefix + correlationId.ToString();
+
+            context.Items[correlationHeader] = correlationId.ToString();
+
+            // Add to response header BEFORE response starts
+            context.Response.OnStarting(() =>
+            {
+                context.Response.Headers[correlationHeader] = correlationId.ToString();
+                return Task.CompletedTask;
+            });
+
+            //pass to the next middleware pipeline pushing correlation id to serilog context
+            using(LogContext.PushProperty("CorrelationId", correlationId))
+            {
+                await _next(context);
+            }
+
+        }
+    }
+}
---
+++ RecoTrackApi/CustomMiddlewares/RequestTimingMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+
+namespace RecoTrackApi.CustomMiddlewares
+{
+    public class RequestTimingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<RequestTimingMiddleware> _logger;
+
+        public RequestTimingMiddleware(RequestDelegate next, ILogger<RequestTimingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var sw = Stopwatch.StartNew();
+            _logger.LogInformation("Start {Path}", context.Request.Path);
+            await _next(context); // forward
+            sw.Stop();
+            _logger.LogInformation("Elapsed {ms}ms for {Path}", sw.ElapsedMilliseconds, context.Request.Path);
+        }
+    }
+
+    public static class RequestTimingExtensions
+    {
+        public static IApplicationBuilder UseRequestTiming(this IApplicationBuilder app) =>
+            app.UseMiddleware<RequestTimingMiddleware>();
+    }
+
+}
