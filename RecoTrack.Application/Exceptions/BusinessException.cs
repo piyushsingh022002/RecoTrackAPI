041c1b833dfe68ea3adcb3d5b97bdebb9a644626
Piyush Singh <workspace.piyush01@gmail.com>
2025-12-19 10:19:56 +0530

    Global Exceptional Middleware added

---
+++ RecoTrack.Application/Exceptions/BusinessException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecoTrack.Application.Exceptions
+{
+    public sealed class BusinessException : AppException
+    {
+        public BusinessException(string message, string errorCode = "BUS-001") : base(message, errorCode) { }
+
+    }
+
+}
---
+++ RecoTrack.Shared/Contracts/Errors/ApiErrorResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecoTrack.Shared.Contracts.Errors
+{
+    public sealed class ApiErrorResponse
+    {
+        public int Status { get; init; }
+        public string ErrorCode { get; init; } = default!;
+        public string Message { get; init; } = default!;
+        public string CorrelationId { get; init; } = default!;
+        public string? ClientId { get; init; }
+        public string Path { get; init; } = default!;
+        public DateTime Timestamp { get; init; } = DateTime.UtcNow;
+
+    }
+}
