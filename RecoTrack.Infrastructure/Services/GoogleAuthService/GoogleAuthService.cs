aed30af93aaec488b5fda424bfa401279553f089
Piyush Singh <piyush@centroxy.com>
2026-02-11 22:46:52 +0530

    changes to me implement more

---
+++ RecoTrack.Infrastructure/Services/GoogleAuthService/GoogleAuthService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Google.Apis.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecoTrack.Infrastructure.Services.GoogleAuthService
+{
+    public class GoogleAuthService
+    {
+        private readonly string _clientId;
+
+        public GoogleAuthService(IConfiguration config)
+        {
+            _clientId = config["GoogleAuth:ClientId"];
+        }
+
+        public async Task<GoogleJsonWebSignature.Payload> VerifyToken(string accessToken)
+        {
+            return await GoogleJsonWebSignature.ValidateAsync(accessToken,
+                new GoogleJsonWebSignature.ValidationSettings
+                {
+                    Audience = new[] { _clientId }
+                });
+        }
+
+        
+    }
+}
